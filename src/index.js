3aa191998ce1e59b6a1983a8190a82b0fa5f5288
Gerardo Leal <gleal@atlassian.com>
2019-01-21 14:24:21 +1100

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import _ from "lodash";
+import "./style.css";
+import Logo from "./logo.png";
+import Data from "./data.xml";
 
 function component() {
   let element = document.createElement("div");
 
+  // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  // Add the image to our existing div
+  var logo = new Image();
+  logo.src = Logo;
+  logo.width = 150;
+  logo.height = 50;
+
+  element.appendChild(logo);
+
+  console.log(Data);
 
   return element;
 }
